1f244e1a5b3c8659cb72ac5f2592bf5ea52bb74b
AugustoValerio <AugustoValerio@boszdigital.com>
2013-01-04 14:56:25 -0600

    waypoints

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -7,6 +7,35 @@ $(function() {
        nav_container.toggleClass('sticky', direction=='down');
     }
   });
+  var sections = $("#sections section");
+	var navigation_links = $("nav.pinnav a");
+	
+	sections.waypoint({
+		handler: function(event, direction) {
+		
+			var active_section;
+			active_section = $(this);
+			if (direction === "up") active_section = active_section.prev();
+
+			var active_link = $('nav.pinnav a[href="#' + active_section.attr("id") + '"]');
+			navigation_links.removeClass("selected");
+			active_link.addClass("selected");
+
+		},
+		offset: '25%'
+	})
+	
+	
+	navigation_links.click( function(event) {
+
+		$.scrollTo(
+			$(this).attr("href"),
+			{
+				duration: 200,
+				offset: { 'left':0, 'top':-0.15*$(window).height() }
+			}
+		);
+	});
 });
 $(function() {
   // Do our DOM lookups beforehand
