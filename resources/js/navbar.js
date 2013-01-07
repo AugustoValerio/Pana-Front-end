fd9dcc3f6bcc7a9de3e76c0c47d78f6a4a2c478e
AugustoValerio <AugustoValerio@boszdigital.com>
2013-01-07 17:50:44 -0600

    navbar grad

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -18,8 +18,8 @@ $(function() {
 			if (direction === "up") active_section = active_section.prev();
 
 			var active_link = $('nav.pinnav a[href="#' + active_section.attr("id") + '"]');
-			
-			active_link.toggleClass("selected");
+			navigation_links.removeClass("selected");
+			active_link.addClass("selected");
 			
 
 		},
@@ -48,4 +48,21 @@ $(function() {
        placeh.toggleClass('on', direction=='down')
     }
   });
+});
+var bar = $('#pinnav'),
+    $window = $(window),
+    docHeight = $(document).height(),
+    baseX = $window.height() / docHeight * 130;
+
+bar.css('background', '-webkit-linear-gradient(45deg, #00a0c6 '+ baseX +'%, #434343 '+ baseX +'%)');
+bar.css('background', '-moz-linear-gradient(45deg, #00a0c6 '+ baseX +'%, #434343 '+ baseX +'%)');
+bar.css('background', '-ms-linear-gradient(45deg, #00a0c6 '+ baseX +'%, #434343 '+ baseX +'%)');
+bar.css('background', 'linear-gradient(45deg, #00a0c6 '+ baseX +'%, #434343 '+ baseX +'%)');
+
+$window.scroll(function(e) {      
+    var x = $window.scrollTop() / docHeight * 160 + baseX;
+    bar.css('background', '-webkit-linear-gradient(45deg, #00a0c6 '+ x +'%, #434343 '+ x +'%)');
+    bar.css('background', '-moz-linear-gradient(45deg, #00a0c6 '+ x +'%, #434343 '+ x +'%)');
+    bar.css('background', '-ms-linear-gradient(45deg, #00a0c6 '+ x +'%, #434343 '+ x +'%)');
+    bar.css('background', 'linear-gradient(45deg, #00a0c6 '+ x +'%, #434343 '+ x +'%)');
 });
