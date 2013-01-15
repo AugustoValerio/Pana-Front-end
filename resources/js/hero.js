04bdf832adb230a6d275c90cbb39edd90a551097
AugustoValerio <AugustoValerio@boszdigital.com>
2013-01-15 05:57:20 -0600

    up out

--- resources/js/hero.js
+++ resources/js/hero.js
@@ -1,19 +1,21 @@
-$("#specsbut").click(function() {
+$(function() {
 
    var docHeight = $(document).height();
-
+$(".overviewbut").click(function(){
    $("body").append("<div id='overlay'></div>");
 
    $("#overlay")
       .height(docHeight)
       .css({
          'opacity' : 0.4,
-         'position': 'absolute',
+         'position': 'fixed',
          'top': 0,
          'left': 0,
          'background-color': 'black',
          'width': '100%',
          'z-index': 5000
       });
-
+      
+    
+});
 });
